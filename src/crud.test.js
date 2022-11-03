256611ac2b5011b37769f36ec28773c91e47a296
aster-alemu <leaster2014w@gmail.com>
2022-11-03 07:19:11 -0700

    Fix linter errors

--- src/crud.test.js
+++ src/crud.test.js
@@ -1,11 +1,12 @@
 const { addTask } = require('./crud');
-test("add a task to the list", () => {
-      TaskManipulation.addTask("Task New", testTasks.getItems());
-      expect(testTasks.getItems().length).toBe(3);
-    });
 
-    test("adds li element to the list", () => { 
-      renderWithEventListeners(testTasks.getItems());
-      const list = document.getElementById("task-list").childNodes.length;
-      expect(list).toBe(3);
-    });
+test('add a task to the list', () => {
+  TaskManipulation.addTask('Task New', testTasks.getItems());
+  expect(testTasks.getItems().length).toBe(3);
+});
+
+test('adds li element to the list', () => {
+  renderWithEventListeners(testTasks.getItems());
+  const list = document.getElementById('task-list').childNodes.length;
+  expect(list).toBe(3);
+});
